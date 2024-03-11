9c043d809c646d8dfd4bb5d2d91998cc5049b4a7
Burak.Kalafat <Burak.Kalafat@insightsoftware.com>
2024-03-11 14:33:16 +0300

    feat: Add test console

--- EventHandlers/HotelCreatedEventHandler.cs
+++ EventHandlers/HotelCreatedEventHandler.cs
@@ -1,9 +1,14 @@
 ﻿using System.Text.Json;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
 using Amazon.Lambda.SNSEvents;
 using HotelCreatedEventHandler.Models;
 using Nest;
+using Amazon;
+
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
 namespace HotelCreatedEventHandler
 {
@@ -11,13 +16,14 @@ namespace HotelCreatedEventHandler
     {
         public async Task Handler(SNSEvent snsEvent)
         {
-            var dbClient = new AmazonDynamoDBClient();
+            Console.WriteLine("Lambda was invoked.");
+            var dbClient = new AmazonDynamoDBClient(RegionEndpoint.EUNorth1);
             var table = Table.LoadTable(dbClient, "hotel-created-event-ids");
 
             
-            var host = Environment.GetEnvironmentVariable("host");//https://96cb61893fd34951b5018633338ab10a.europe-west3.gcp.cloud.es.io:443
-            var userName = Environment.GetEnvironmentVariable("userName");//elastic               
-            var password = Environment.GetEnvironmentVariable("password");//Pc5ci6F4AY0251c55ZIbqKi7
+            var host = Environment.GetEnvironmentVariable("host");
+            var userName = Environment.GetEnvironmentVariable("userName");
+            var password = Environment.GetEnvironmentVariable("password");
             var indexName = Environment.GetEnvironmentVariable("indexName");
 
             var connSettings = new ConnectionSettings(new Uri(host));
@@ -27,26 +33,24 @@ namespace HotelCreatedEventHandler
 
             var esClient = new ElasticClient(connSettings);
 
-            if (!(await esClient.Indices.ExistsAsync(indexName)).Exists)
-            {
-                await esClient.Indices.CreateAsync(indexName);
-            }
+            if (!(await esClient.Indices.ExistsAsync(indexName)).Exists) await esClient.Indices.CreateAsync(indexName);
+
+            Console.WriteLine($"Found {snsEvent.Records.Count} records in SNS Event");
 
             foreach (var eventRecord in snsEvent.Records)
             {
                 var eventId = eventRecord.Sns.MessageId;
-                var foundItem = table.GetItemAsync(eventId);
+                var foundItem = await table.GetItemAsync(eventId);
                 if (foundItem == null)
-                {
                     await table.PutItemAsync(new Document
                     {
                         ["eventId"] = eventId
                     });
-                }
 
                 var hotel = JsonSerializer.Deserialize<Hotel>(eventRecord.Sns.Message);
 
-                await esClient.IndexDocumentAsync<Hotel>(hotel);
+                var response = await esClient.IndexDocumentAsync<Hotel>(hotel);
+                if (response.Result == Result.Error) Console.WriteLine($"Server Error:{response.ServerError.Error.Reason}");
             }
         }
     }
---
+++ HotelCreatedEventHandler.Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Amazon.Lambda.SNSEvents;
+using HotelCreatedEventHandler.Models;
+
+//TEST URL PASS
+Environment.SetEnvironmentVariable("host", "https://96cb61893fd34951b5018633338ab10a.europe-west3.gcp.cloud.es.io:443");
+Environment.SetEnvironmentVariable("userName", "elastic");
+Environment.SetEnvironmentVariable("password", "Pc5ci6F4AY0251c55ZIbqKi7");
+Environment.SetEnvironmentVariable("indexName", "event");
+
+var hotel = new Hotel
+{
+    Name = "Continental",
+    City = "Paris",
+    Price = 100,
+    Rating = 4,
+    Id = "123",
+    UserId = "ABC",
+    CreationDateTime = DateTime.Now
+};
+
+var snsEvent = new SNSEvent
+{
+    Records = new List<SNSEvent.SNSRecord>
+    {
+        new SNSEvent.SNSRecord()
+        {
+            Sns = new SNSEvent.SNSMessage
+            {
+                MessageId = "100",
+                Message = JsonSerializer.Serialize(hotel)
+            }
+        }
+    }
+};
+
+var handler = new HotelCreatedEventHandler.HotelCreatedEventHandler();
+await handler.Handler(snsEvent);
